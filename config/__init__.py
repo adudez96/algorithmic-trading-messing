bf2cfa00f47166867f434f95e44ec6eee2c467e2
Aman Singh <adudez96@gmail.com>
2021-03-07 19:37:14 +1100

    basic file + stdout logging

--- config/__init__.py
+++ config/__init__.py
@@ -1,9 +1,10 @@
 import hiyapyco
 
+import logging
 import os
 
 env = os.environ.get('APP_ENV', default="local")
 
 config = hiyapyco.load('{}/config/_base.yaml'.format(os.getcwd()), '{}/config/{}.yaml'.format(os.getcwd(), env))
 
-print(hiyapyco.dump(config))
+logging.debug(hiyapyco.dump(config))
--- main.py
+++ main.py
@@ -2,11 +2,16 @@ from config import config
 
 from google.cloud import bigquery, storage
 
+import logging
+import sys
+
+
 def main() -> None:
-    print("Running!")
+    setup_logging()
+    logging.info("Running!")
     # run_bigquery_client_test()
     # run_storage_client_test()
-    print(get_ameritrade_creds())
+    logging.debug(get_ameritrade_creds())
 
 
 def get_ameritrade_creds() -> str:
@@ -16,6 +21,25 @@ def get_ameritrade_creds() -> str:
     api_key = bucket.get_blob(config["storage"]["tdameritrade-api-creds"]["blob"]).download_as_string()
 
     return api_key
+
+
+def setup_logging() -> None:
+    # Clear all logging handlers to make way for custom
+    logging.getLogger().handlers = []
+
+    log_format = '[%(asctime)s] [%(levelname)s] {%(filename)s:%(lineno)d} %(message)s'
+    formatter = logging.Formatter(log_format)
+
+    file_handler = logging.FileHandler(filename='tmp.log')
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.getLevelName(config["logging"]["file-level"]))
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setFormatter(formatter)
+    stdout_handler.setLevel(logging.getLevelName(config["logging"]["stdout-level"]))
+
+    logging.getLogger().setLevel(logging.NOTSET)
+    logging.getLogger().addHandler(file_handler)
+    logging.getLogger().addHandler(stdout_handler)
 
 
 def run_bigquery_client_test() -> None:
@@ -30,7 +54,7 @@ def run_bigquery_client_test() -> None:
     rows = query_job.result()  # Waits for query to finish
 
     for row in rows:
-        print(row)
+        logging.debug(row)
 
 
 def run_storage_client_test() -> None:
@@ -40,7 +64,7 @@ def run_storage_client_test() -> None:
 
     # Make an authenticated API request
     buckets = list(storage_client.list_buckets())
-    print(buckets)
+    logging.debug(buckets)
 
 
 if __name__ == "__main__":
